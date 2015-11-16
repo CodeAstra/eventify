27ff6628500425e5e115719a4cb6030cc77a0225
B V Satyaram <bvsatyaram@gmail.com>
2015-11-16 11:36:26 +0530

    Introduce shwo page for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,10 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
 private
   def event_params
     params.require(:event).permit(:title, :description)
